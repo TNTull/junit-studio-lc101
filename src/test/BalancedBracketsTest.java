15f5c0217c79b51236a25b94664d5a000770ec05
Teresa <teresatull@comcast.net>
2022-10-01 06:27:31 -0500

    ch 6 studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,9 @@
+//Teresa Tull worked on this on 10-1-2022 in class for ch 6 studio
+
 package test;
 
 import org.junit.Test;
+import main.BalancedBrackets;
 
 import static org.junit.Assert.*;
 
@@ -12,5 +15,64 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("{}"));
+    }
 
+    @Test
+    public void bracketsWithWordReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void bracketsWithinWordReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void bracketsBeforeStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void noBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void openBracketWithWordsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void backwardsBracketsWithWordReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void openBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void backwardsBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void stringWithoutBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Our nest is best"));
+    }
+
+    @Test
+    public void closedBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void oddNumberOfBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]["));
+    }
 }
+
